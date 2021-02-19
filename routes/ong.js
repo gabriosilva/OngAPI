5baf3ecde672c9eddff103971391e481714c7a35
gabrio Lina da Silva <MexSolutions@outlook.com>
2021-02-19 17:32:01 -0300

    # Fixed Ong.save()

--- routes/ong.js
+++ routes/ong.js
@@ -5,25 +5,27 @@ router.post('/addOng',async(req,res)=>{
     
     let errorArray = [];
     let success = true;
-    try{
-        const ongObj = new Ong({
-            name: req.body.name,
-            description: req.body.description,
-            identificationNumber: req.body.identificationNumber,
-            email: req.body.email,
-            phone: req.body.phone,
-            website: req.body.website,
-            type:req.body.type,
-            causes:req.body.causes,
-            country:req.body.country,
-            zipcode:req.body.zipcode,
-            state:req.body.state,
-            city:req.body.city,
-            addressNumber: req.body.addressNumber,
-            bornIn: req.body.bornIn
-        });
 
-        const sentOng = await Ong.save();
+    //Create the ong obj
+    const ongObj = new Ong({
+        name: req.body.name,
+        description: req.body.description,
+        identificationNumber: req.body.identificationNumber,
+        email: req.body.email,
+        phone: req.body.phone,
+        website: req.body.website,
+        type:req.body.type,
+        causes:req.body.causes,
+        country:req.body.country,
+        zipcode:req.body.zipcode,
+        state:req.body.state,
+        city:req.body.city,
+        addressNumber: req.body.addressNumber,
+        bornIn: req.body.bornIn
+    });
+
+    try{
+        const sentOng = await ongObj.save();
         const responseObj = {
             data:[sentOng],
             success:success,
