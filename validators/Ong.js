0a20233f4a9cfc6cc8df580505ac6373f1d38599
gabrio Lina da Silva <MexSolutions@outlook.com>
2021-02-20 21:18:42 -0300

    #added getOng validator

--- validators/Ong.js
+++ validators/Ong.js
@@ -22,4 +22,23 @@ const addOngValidation = data => {
     return schema.validate(data);
 }
 
+const getOngValidation = data =>{
+    const schema = Joi.object({
+        _id: Joi.string(),
+        identificationNumber:Joi.number().min(0).max(999999999999999999999),
+        email:Joi.string().min(2).max(255).email(),
+        phone:Joi.number().min(0).max(999999999999999999999),
+        type:Joi.number().min(0).max(999999999),
+        causes:Joi.number().min(0).max(999999999),
+        country: Joi.string().min(2).max(255),
+        state: Joi.string().min(2).max(255),
+        city: Joi.string().min(2).max(255),
+        addressNumber: Joi.string().min(0).max(255),
+        bornIn: Joi.date()
+    })
+
+    return schema.validate(data);
+}
+
 module.exports.addOngValidation = addOngValidation;
+module.exports.getOngValidation = getOngValidation;
